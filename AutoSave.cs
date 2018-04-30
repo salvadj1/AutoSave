b9cb1f4392ccd70ceea812534e6074dd28a72a49
salva <24988418+salvadj1@users.noreply.github.com>
2018-04-30 17:47:05 +0200

    1.5

--- AutoSave.cs
+++ AutoSave.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 
 using System.ComponentModel;
+using Fougerite.Events;
 using Fougerite;
 using UnityEngine;
-using RustPP;
 
-using Fougerite.Events;
-using UnityEngine.Events;
-using System.Timers;
+using RustProto;
+using RustProto.Helpers;
+using System.IO;
 
 namespace AutoSave
 {
@@ -19,37 +19,41 @@ namespace AutoSave
         public override string Name { get { return "AutoSave"; } }
         public override string Author { get { return "Salva/juli"; } }
         public override string Description { get { return "AutoSave "; } }
-        public override Version Version { get { return new Version("1.3"); } }
-        public static string Description2 = "[AutoSave v.1.3] ";
+        public override Version Version { get { return new Version("1.5"); } }
 
-        private string red = "[color #FF0000]";
-        private string blue = "[color #81F7F3]";
-        private string green = "[color #82FA58]";
-        private string yellow = "[color #F4FA58]";
-        private string orange = "[color #FF8000]";
-
-        public static int count = 0;
+        private static string red = "[color #FF0000]";
+        private static string blue = "[color #81F7F3]";
+        private static string green = "[color #82FA58]";
+        private static string yellow = "[color #F4FA58]";
+        private static string orange = "[color #FF8000]";
+        private static string white = "[color #FFFFFF]";
 
         public Mono MonoClass;
         public GameObject monoClassLoad;
+        public IniParser Settings;
 
-        public static BackgroundWorker saveA;
-        
+        public static DateTime SaveStartTime = DateTime.Now;
+        public static DateTime SaveEndTime = DateTime.Now;
+        public static int ObjectsCount = 0;
+
+        public static int MinsToSave = 10;
+        public static bool SaveInBackground = false;
+        public static bool ShowObjectCount = true;
+
+        public static BackgroundWorker BGW;
+
         public override void Initialize()
-        {     
-            Hooks.OnServerInit += OnServerInit;
+        {
+            Hooks.OnServerLoaded += OnServerLoaded;
             Hooks.OnConsoleReceived += OnConsoleReceived;
-
-            monoClassLoad = new GameObject();
-            MonoClass = monoClassLoad.AddComponent<Mono>();
-            UnityEngine.Object.DontDestroyOnLoad(monoClassLoad);
+            Hooks.OnServerInit += OnServerInit;
+            ReloadConfig();
         }
         public override void DeInitialize()
         {
-            Hooks.OnServerInit -= OnServerInit;
+            Hooks.OnServerLoaded -= OnServerLoaded;
             Hooks.OnConsoleReceived -= OnConsoleReceived;
-            saveA.DoWork -= new DoWorkEventHandler(GuardarMapaA);
-
+            Hooks.OnServerInit -= OnServerInit;
             if (monoClassLoad != null) UnityEngine.Object.DestroyImmediate(monoClassLoad);
         }
         public void OnServerInit()
@@ -57,13 +61,215 @@ namespace AutoSave
             //ConsoleSystem.Run("save.autosavetime 999999999", false); //disable Native Server Save
             ConsoleSystem.Run("save.autosavetime " + int.MaxValue, false);//dretax way
         }
+        public void OnServerLoaded()
+        {
+            monoClassLoad = new GameObject();
+            MonoClass = monoClassLoad.AddComponent<Mono>();
+            UnityEngine.Object.DontDestroyOnLoad(monoClassLoad);
+        }
         public void OnConsoleReceived(ref ConsoleSystem.Arg arg, bool external)
         {
-            if (arg.Class == "autosave" && arg.Function == "status" && ((arg.argUser != null && arg.argUser.admin) || arg.argUser == null))
+            if (arg.Class == "help" && arg.Function == "help" && ((arg.argUser != null && arg.argUser.admin) || arg.argUser == null))
             {
-                ConsoleSystem.LogError("AutoSave is Bussy?: " + saveA.IsBusy.ToString());
+                Logger.LogError("PLUGIN: " + Name + " " + Version);
+                Logger.Log("asave.save - SAVE ALL MAP");
+                Logger.Log("asave.reload - RELOAD CONFIGURATION");
+                Logger.LogError("");
+            }
+            if (arg.Class == "asave" && arg.Function == "save" && ((arg.argUser != null && arg.argUser.admin) || arg.argUser == null))
+            {
+                CallServerSave();
+                Logger.Log("Done!");
+            }
+            if (arg.Class == "asave" && arg.Function == "reload" && ((arg.argUser != null && arg.argUser.admin) || arg.argUser == null))
+            {
+                ReloadConfig();
+                Logger.Log("Done!");
             }
         }
+        private void ReloadConfig()
+        {
+            if (!File.Exists(Path.Combine(ModuleFolder, "Settings.ini")))
+            {
+                File.Create(Path.Combine(ModuleFolder, "Settings.ini")).Dispose();
+                Settings = new IniParser(Path.Combine(ModuleFolder, "Settings.ini"));
+                Settings.AddSetting("Settings", "MinsToSave", "10");
+                Settings.AddSetting("Settings", "SaveInBackground", "false");
+                Settings.AddSetting("Settings", "ShowObjectCount", "true");
+                Settings.Save();
+                Logger.Log(Name + " Plugin: New Settings File Created!");
+                ReloadConfig();
+            }
+            else
+            {
+                Settings = new IniParser(Path.Combine(ModuleFolder, "Settings.ini"));
+                if (Settings.ContainsSetting("Settings", "MinsToSave") &&
+                    Settings.ContainsSetting("Settings", "SaveInBackground") &&
+                    Settings.ContainsSetting("Settings", "ShowObjectCount"))
+                {
+
+                    try
+                    {
+                        MinsToSave = int.Parse(Settings.GetSetting("Settings", "MinsToSave"));
+                        SaveInBackground = Settings.GetBoolSetting("Settings", "SaveInBackground");
+                        ShowObjectCount = Settings.GetBoolSetting("Settings", "ShowObjectCount");
+                        Logger.Log(Name + " Plugin: Settings file Loaded!");
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.Log(Name + " Plugin: Detected a problem in the configuration");
+                        Logger.Log("ERROR -->" + ex.Message);
+                        File.Delete(Path.Combine(ModuleFolder, "Settings.ini"));
+                        Logger.Log(Name + " Plugin: Deleted the old configuration file");
+                        ReloadConfig();
+                    }
+                }
+                else
+                {
+                    Logger.LogError(Name + " Plugin: Detected a problem in the configuration (lost key)");
+                    File.Delete(Path.Combine(ModuleFolder, "Settings.ini"));
+                    Logger.LogError(Name + " Plugin: Deleted the old configuration file");
+                    ReloadConfig();
+                }
+                return;
+            }
+            return;
+        }
+        public static void CallServerSave()
+        {
+            Loom.QueueOnMainThread(() =>
+            {
+                SaveStartTime = DateTime.Now;
+
+                if (!Directory.Exists(Directory.GetCurrentDirectory() + @"\save\server_data\BackUpSaves\"))
+                {
+                    Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\save\server_data\BackUpSaves\");
+                }
+                try
+                {
+                    if (File.Exists(Directory.GetCurrentDirectory() + @"\save\server_data\rust_island_2013.sav"))
+                    {
+                        string d = System.DateTime.Now.Day.ToString();
+                        string h = System.DateTime.Now.Hour.ToString();
+                        string m = System.DateTime.Now.Minute.ToString();
+                        string s = System.DateTime.Now.Second.ToString();
+                        string date = "Day " + d + " Hour " + h + "-" + m + "-" + s;
+                        string name = "rust_island_2013 " + date + ".sav";
+                        File.Copy(Directory.GetCurrentDirectory() + @"\save\server_data\rust_island_2013.sav", Directory.GetCurrentDirectory() + @"\save\server_data\BackUpSaves\" + name, true);
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+
+
+                if (SaveInBackground)
+                {
+                    BackgroundSave();
+                }
+                else
+                {
+                    NormalSave();
+                }
+            });
+        }
+
+        public static void NormalSave()
+        {
+            Loom.QueueOnMainThread(() =>
+            {
+                AvatarSaveProc.SaveAll(); //???????
+                WorldSave fsave;
+                using (Recycler<WorldSave, WorldSave.Builder> recycler = WorldSave.Recycler())
+                {
+                    WorldSave.Builder builder = recycler.OpenBuilder();
+                    ServerSaveManager.Get(false).DoSave(ref builder);
+                    fsave = builder.Build();
+                }
+
+                if (ShowObjectCount)
+                {
+                    ObjectsCount = fsave.SceneObjectCount + fsave.InstanceObjectCount;
+                }
+
+                FileStream stream2 = File.Open(Directory.GetCurrentDirectory() + @"\save\server_data\rust_island_2013.sav", FileMode.Create, FileAccess.Write);
+                fsave.WriteTo(stream2);
+                stream2.Flush();
+                
+                stream2.Dispose(); //??
+
+                SaveEndTime = DateTime.Now;
+                AnnounceResults();
+            }); 
+        }
+
+        public static void BackgroundSave()
+        {
+            //BackgroundWorker BGW = new BackgroundWorker();
+            BGW = new BackgroundWorker();
+            BGW.DoWork += new DoWorkEventHandler(SaveBW);
+            BGW.RunWorkerAsync();
+        }
+        public static void SaveBW(object sender, DoWorkEventArgs e)
+        {
+            AvatarSaveProc.SaveAll(); //???????
+            WorldSave fsave;
+            using (Recycler<WorldSave, WorldSave.Builder> recycler = WorldSave.Recycler())
+            {
+                WorldSave.Builder builder = recycler.OpenBuilder();
+                ServerSaveManager.Get(false).DoSave(ref builder);
+                fsave = builder.Build();
+            }
+
+            if (ShowObjectCount)
+            {
+                ObjectsCount = fsave.SceneObjectCount + fsave.InstanceObjectCount;
+            }
+
+            FileStream stream2 = File.Open(Directory.GetCurrentDirectory() + @"\save\server_data\rust_island_2013.sav", FileMode.Create, FileAccess.Write);
+            fsave.WriteTo(stream2);
+            stream2.Flush();
+            stream2.Dispose(); //??
+
+            SaveEndTime = DateTime.Now;
+            AnnounceResults();
+            BGW.Dispose();
+        }
+        public static void AnnounceResults()
+        {
+            string savetype = " ";
+            if (SaveInBackground)
+            {
+                savetype = "in Background";
+            }
+            Loom.QueueOnMainThread(() =>
+            {
+                TimeSpan DifTime = SaveEndTime.Subtract(SaveStartTime);
+
+                if (ShowObjectCount)
+                {
+                    Logger.Log(" ");
+                    Logger.Log("Server Saved " + ObjectsCount + " Object(s). Took " + DifTime.Seconds + "." + DifTime.Milliseconds + " seconds to save them " + savetype);
+                    Logger.Log(@"BACKUP file has been created in ...\save\server_data\BackUpSave");
+                    Logger.Log(" ");
+
+                    Server.GetServer().BroadcastFrom("AutoSave", "Server Saved " + yellow + ObjectsCount + white +
+                        " Object(s). Took " + yellow + DifTime.Seconds + "." + DifTime.Milliseconds + white + " seconds " + savetype);
+                }
+                else
+                {
+                    Logger.Log(" ");
+                    Logger.Log("Server Saved Took " + DifTime.Seconds + "." + DifTime.Milliseconds + " seconds " + savetype);
+                    Logger.Log(@"BACKUP file has been created in ...\save\server_data\BackUpSave");
+                    Logger.Log(" ");
+
+                    Server.GetServer().BroadcastFrom("AutoSave", "Server Saved Took " +
+                        yellow + DifTime.Seconds + "." + DifTime.Milliseconds + white + " seconds " + savetype);
+                }
+            });
+            return;
+        }
+        /*
         public static void ContarObjectos()
         {
             if (Util.GetUtil().CurrentWorkingThreadID != Util.GetUtil().MainThreadID)
@@ -83,21 +289,6 @@ namespace AutoSave
                 Logger.Log("Error Autosave" + ex.ToString());
             }
             return;
-        }
-        public static void GuardarMapaA(object sender, DoWorkEventArgs e)
-        {
-            var StartTime = DateTime.Now;
-
-            ConsoleSystem.Run("save.all", false);
-
-            var EndTime = DateTime.Now;
-            TimeSpan DifTime = EndTime.Subtract(StartTime);
-
-            Logger.Log(Description2 + count + " Object(s). Took " + DifTime.Seconds + "." + DifTime.Milliseconds + " seconds save them in the Background");
-            ConsoleSystem.Print(Description2 + count + " Object(s). Took " + DifTime.Seconds + "." + DifTime.Milliseconds + " seconds save them in the Background");
-            Server.GetServer().BroadcastFrom(Description2, count + " Object(s). Took " + DifTime.Seconds + "." + DifTime.Milliseconds + " seconds save them in the Background");
-
-            saveA.CancelAsync();
-        }     
+        }*/
     }
 }
